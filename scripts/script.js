1c8e19119ce4295498600a2dfb4a0e7f837c5f1a
Daniel Bartelli <danielbartelli27@gmail.com>
2024-03-18 10:37:59 -0300

    aad botao pra consultar preço total

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,33 @@
 var carrinho = [];
 
-
-function consultarPreco() {
-    var produtoInput = document.getElementById('produtoInput').value;
-    
-    var preco = Math.random() * (100 - 1) + 1; 
-    document.getElementById('resultado').innerText = "Preço do produto '" + produtoInput + "': R$ " + preco.toFixed(2);
-}
-
-
-function comprarProduto() {
-
-    var produtoInput = document.getElementById('produtoInput').value;
-    var preco = Math.random() * (100 - 1) + 1;
-    var produto = { nome: produtoInput, preco: preco };
-
-    carrinho.push(produto);
-    document.getElementById('resultado').innerText = "Produto '" + produtoInput + "' adicionado ao carrinho!";
-    console.log("Carrinho:", carrinho);
-}
-
-
-function calcularTotal() {
-
-    var soma = 0;
-    
-    for (var i = 0; i < carrinho.length; i++) {
-        soma += carrinho[i].preco;
+    function consultarPreco() {
+        var produtoInput = document.getElementById('produtoInput').value;
+        
+        var preco = Math.random() * (100 - 1) + 1; 
+        document.getElementById('resultado').innerText = "Preço do produto '" + produtoInput + "': R$ " + preco.toFixed(2);
     }
-    
-    var resultado = soma > 0 ? "Total do Carrinho: R$ " + soma.toFixed(2) : "Carrinho vazio";
-    document.getElementById('resultado').innerText = resultado;
-}
+
+
+    function comprarProduto() {
+        var produtoInput = document.getElementById('produtoInput').value;
+        
+        var preco = Math.random() * (100 - 1) + 1; 
+        var produto = { nome: produtoInput, preco: preco };
+        carrinho.push(produto);
+        document.getElementById('resultado').innerText = "Produto '" + produtoInput + "' adicionado ao carrinho!";
+        console.log("Carrinho:", carrinho);
+
+       
+        document.getElementById('numProdutos').innerText = carrinho.length;
+    }
+
+    function calcularTotal() {
+        var soma = 0;
+        
+        for (var i = 0; i < carrinho.length; i++) {
+            soma += carrinho[i].preco;
+        }
+        
+        var resultado = soma > 0 ? "R$ " + soma.toFixed(2) : "Carrinho vazio";
+        document.getElementById('valorTotal').innerText = resultado;
+    }
