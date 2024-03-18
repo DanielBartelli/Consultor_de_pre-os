c3cc8135a9601dac792d5b5c201fad8fe5389f46
Daniel Bartelli <danielbartelli27@gmail.com>
2024-03-18 11:05:00 -0300

    add localsotage

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,45 @@
 var carrinho = [];
 
-function consultarPreco() {
-    var produtoInput = document.getElementById('produtoInput').value;
-    
-    var preco = Math.random() * (100 - 1) + 1; 
-    document.getElementById('resultado').innerText = "Preço do produto '" + produtoInput + "': R$ " + preco.toFixed(2);
-}
-
-function comprarProduto() {
-    var produtoInput = document.getElementById('produtoInput').value;
-  
-    
-    var preco = Math.random() * (100 - 1) + 1;
-    var produto = { nome: produtoInput, preco: preco };
-    carrinho.push(produto);
-    document.getElementById('resultado').innerText = "Produto '" + produtoInput + "' adicionado ao carrinho!";
-    console.log("Carrinho:", carrinho);
-
-    atualizarNumProdutos();
-    atualizarValorTotal();
-}
-
-function atualizarNumProdutos() {
-    document.getElementById('numProdutos').innerText = carrinho.length;
-}
-
-function atualizarValorTotal() {
-    var soma = 0;
-    
-    for (var i = 0; i < carrinho.length; i++) {
-        soma += carrinho[i].preco;
+    if (localStorage.getItem('carrinho')) {
+        carrinho = JSON.parse(localStorage.getItem('carrinho'));
+        atualizarNumProdutos();
+        atualizarValorTotal();
     }
-    
-    document.getElementById('valorTotal').innerText = "R$ " + soma.toFixed(2);
-}
+
+    function consultarPreco() {
+        var produtoInput = document.getElementById('produtoInput').value;
+        
+        var preco = Math.random() * (100 - 1) + 1; 
+        document.getElementById('resultado').innerText = "Preço do produto '" + produtoInput + "': R$ " + preco.toFixed(2);
+    }
+
+
+    function comprarProduto() {
+        var produtoInput = document.getElementById('produtoInput').value;
+       
+        var preco = Math.random() * (100 - 1) + 1; 
+        var produto = { nome: produtoInput, preco: preco };
+        carrinho.push(produto);
+        document.getElementById('resultado').innerText = "Produto '" + produtoInput + "' adicionado ao carrinho!";
+        console.log("Carrinho:", carrinho);
+
+       
+        atualizarNumProdutos();
+        atualizarValorTotal();
+
+        localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    }
+
+    function atualizarNumProdutos() {
+        document.getElementById('numProdutos').innerText = carrinho.length;
+    }
+
+    function atualizarValorTotal() {
+        var soma = 0;
+        
+        for (var i = 0; i < carrinho.length; i++) {
+            soma += carrinho[i].preco;
+        }
+        
+        document.getElementById('valorTotal').innerText = "R$ " + soma.toFixed(2);
+    }
