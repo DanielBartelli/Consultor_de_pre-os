6ca3d73a9576735485e7c029872b3cf80f5bf16b
Daniel Bartelli <danielbartelli27@gmail.com>
2024-03-18 10:14:27 -0300

    botão para consultar o valor total da compra

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,34 @@
+var carrinho = [];
+
+
 function consultarPreco() {
-    var produtoInput = document.getElementById('produto-Input').value;
-    var preco = Math.random() * (100 - 1) + 1;
-    document.getElementById('res').innerText = "Preço do produto '" + produto-Input + "': R$ " + preco.toFixed(2);
+    var produtoInput = document.getElementById('produtoInput').value;
+    
+    var preco = Math.random() * (100 - 1) + 1; 
+    document.getElementById('resultado').innerText = "Preço do produto '" + produtoInput + "': R$ " + preco.toFixed(2);
 }
 
 
 function comprarProduto() {
-    var produtoInput = document.getElementById('produto-Input').value;
-    document.getElementById('res').innerText = "Produto '" + produto-Input + "' comprado com sucesso!";
+
+    var produtoInput = document.getElementById('produtoInput').value;
+    var preco = Math.random() * (100 - 1) + 1;
+    var produto = { nome: produtoInput, preco: preco };
+
+    carrinho.push(produto);
+    document.getElementById('resultado').innerText = "Produto '" + produtoInput + "' adicionado ao carrinho!";
+    console.log("Carrinho:", carrinho);
+}
+
+
+function calcularTotal() {
+
+    var soma = 0;
+    
+    for (var i = 0; i < carrinho.length; i++) {
+        soma += carrinho[i].preco;
+    }
+    
+    var resultado = soma > 0 ? "Total do Carrinho: R$ " + soma.toFixed(2) : "Carrinho vazio";
+    document.getElementById('resultado').innerText = resultado;
 }
